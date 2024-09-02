331ec60cafc6dec1fd0ce705a4af0579e8ea08e8
Haris Sabanovic <hasa2303@student.miun.se>
2024-09-02 12:53:39 +0200

    Lade till eventlyssnare för att hantera borttagning av bokningar

--- src/js/booking.js
+++ src/js/booking.js
@@ -3,9 +3,14 @@ document.addEventListener("DOMContentLoaded", function () {
     fetchBookings();
 });
 
-async function fetchBookings() {
-    const bookingList = document.getElementById("bookingList"); // Antag att du har ett element med detta ID för att visa bokningarna
+document.addEventListener("click", function(event) {
+    if(event.target.matches (".delete-item-class")) {
+        deleteBooking(event.target.dataset.id);
+    }
+})
 
+async function fetchBookings() {
+    const bookingList = document.getElementById("bookingList"); 
     try {
         const response = await fetch("http://localhost:5000/api/booking");
         const bookings = await response.json();
@@ -22,13 +27,34 @@ async function fetchBookings() {
                 <p>Telefonnummer: ${booking.phoneNumber}</p>
                 <p>Datum och tid: ${new Date(booking.bookingDateTime).toLocaleString()}</p>
                 <p>Antal personer: ${booking.amountPeople}</p>
-                <button onclick="editBooking('${booking._id}')">Redigera</button>
-                <button onclick="deleteBooking('${booking._id}')">Ta bort</button>
+                <button class="delete-item-class" data-id="${booking._id}">Ta bort</button>
             `;
 
             bookingList.appendChild(bookingDiv);
         });
     } catch (error) {
-        console.error("Fel vid hämtning av bokningarna:", error);
+        console.error("Could not get the bookings", error);
     }
+}
+
+
+async function deleteBooking(id) {
+
+    if(confirm("Är du säker att du vill ta bort bokning?")) {
+        try {
+            const response = await fetch(`http://localhost:5000/api/booking/${id}`, {
+                method: "DELETE"
+            });
+
+            if(!response.ok) {
+                throw new Error("Kunde inte hämta bokning");
+            }
+
+            fetchBookings();
+
+        } catch(error) {
+            console.error("Fel vid radering av bokning: " + error);
+        }
+    }
+
 }
