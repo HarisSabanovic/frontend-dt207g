a4a820fdbef64691fd5a6888f6a354ab964a0aa9
Haris Sabanovic <hasa2303@student.miun.se>
2024-09-03 13:42:12 +0200

    kommentarer

--- src/js/login.js
+++ src/js/login.js
@@ -1,4 +1,4 @@
-// js/login.js
+//eventlyssnare som körs när användaren skickar formulär data
 document.getElementById("login-form").addEventListener("submit", async function(event) {
     event.preventDefault();
 
@@ -6,13 +6,14 @@ document.getElementById("login-form").addEventListener("submit", async function(
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
+    //POST förfrågan till API för att kunna logga in
     try {
         const response = await fetch("https://projekt-ethique.onrender.com/api/login", {
             method: "POST",
             headers: {
-                "Content-Type": "application/json"
+                "Content-Type": "application/json" //JSON data som skickas
             },
-            body: JSON.stringify({ username, password })
+            body: JSON.stringify({ username, password }) // de användarnamn och lösenord som skickas
         });
 
         if (!response.ok) {
@@ -21,39 +22,46 @@ document.getElementById("login-form").addEventListener("submit", async function(
 
         const data = await response.json();
         const token = data.token;
+        //sparar token i localstorage
         localStorage.setItem("token", token);
 
+
+        //skickar token till funktion för att verifiera
         const isValid = await verifyToken(token);
 
-        // Om token är giltig, omdirigera användaren
+        // Om isValid returnerar true, då är token giltig, omdirigera användaren
         if (isValid) {
             window.location.href = "/admins.html";
         } else {
             throw new Error("Ogiltig token");
         }
 
+        //fel hantering
     } catch (error) {
         console.log("Server fel inträffade: " + error.message);
     }
 });
 
+//funktion som verifierar token
 async function verifyToken(token) {
-    console.log(token);
+ 
 
     try {
+
+        //GET begäran till en skyddad resurs med token som Authorization header
         const response = await fetch('https://projekt-ethique.onrender.com/api/protected', {
             headers: {
-                'Authorization': 'Bearer ' + token
+                'Authorization': 'Bearer ' + token //lägger till token som en bearer-token
             }
         });
     
         if (!response.ok) {
             throw new Error("Access denied");
         }
-        return true; // Token är giltig om allt gick bra
+        return true; //ifall token är giltig 
 
     } catch (err) {
         console.error("Ett fel inträffade vid verifiering av token:", err.message);
-        return false; // Token är ogiltig eller servern avvisade begäran
+        return false; // token är ogiltig eller servern avvisade begäran
     }
 }
